5cb55a2473e9939c6db09d65d098ab01a6c5deb3
DanielMontoya85 <danielmontoya@syscon.com.co>
2025-10-24 12:23:01 -0500

    cambio en model user

--- Models/User.cs
+++ Models/User.cs
@@ -1,11 +1,19 @@
-﻿namespace ApiComederoPet.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiComederoPet.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required, MaxLength(50)]
         public string Username { get; set; }
+
+        [Required, MaxLength(100)] 
         public string Password { get; set; }
     }
 }
+
 
 
